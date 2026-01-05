54b35186fba3bee7f3b386a71c77f5348f16b126
Suresh Pun Magar <sureshmagar6256407@gmail.com>
2026-01-05 16:08:24 +0545

    updae

--- 10-polymorphism/2-practice.py
+++ 10-polymorphism/2-practice.py
@@ -13,4 +13,25 @@ class Circle :
 c1  =Circle(21)
 print(c1.Area())
 print(c1.perimeter())
+
 """
+
+class Employe : 
+    def __init__(self,roll ,department, salary):
+        self.roll   =roll
+        self.department = department
+        self.salary = salary
+
+    def Shodetainl(self) : 
+        print(f"roll = {self.roll}")
+        print(f"department = {self.department}")
+        print(f"salary = {self.salary}")
+
+class Enginer(Employe)    :  
+    def __init__(self, roll, department, salary, name ,age):
+        super().__init__("enginer" ,"bit" , 3000)
+        self.name =name 
+        self.age  =age 
+
+e1 = Enginer("cheerman" , "BIT" ,40000 , "tekam",20)
+e1.Shodetainl()
